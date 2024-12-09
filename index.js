05a6075171df5a173be0c54960efbd5bdef8b7af
sreya81 <sreyakachakayala06@gmail.com>
2024-12-09 23:25:50 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+// Q-1
+function greet(){
+    console.log("Hello!");
+}
+greet();
+console.log("..................................");
+
+// Q-2
+
+function showMessage(){
+    console.log("This is a message!")
+}
+showMessage();
+console.log(".................................");
+
+// Q-3
+
+function logInfo(){
+    console.log("Logging Information…")
+}
+logInfo();
+console.log("...................................");
+
+// Q-4
+
+function startGame(){
+    console.log("Game Started!");
+    endGame();
+}
+function endGame(){
+    console.log("Game Over!");
+}
+startGame();
+console.log(".......................................");
+
+// Q-5
+function beginTask(){
+    console.log("Task Started");
+    finishTask();
+}
+function finishTask(){
+    console.log("Task Finished");
+}
+beginTask();
